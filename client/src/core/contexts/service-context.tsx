c977d4b7dedc7cda6e97751d4bfad7ffee04e593
Matt Schiffman <>
2022-03-21 13:18:10 -0400

    cleanup

--- client/src/core/contexts/service-context.tsx
+++ client/src/core/contexts/service-context.tsx
@@ -1,4 +1,4 @@
-import React, { createContext } from "react"
+import React, { createContext } from "react";
 
 export function makeServiceProviderProps(actions:any, data:any, storage:any) {
     return {actions, data, storage}
@@ -8,10 +8,10 @@ export const ServiceContext = createContext({});
 
 export const ServiceContextProvider = (props:any) => {
     const value = {
-      actions: props.value.actionServices,
-      auth: props.value.authService,
+      // actions: props.value.actionServices,
+      // auth: props.value.authService,
       data: props.value.dataService,
-      storage: props.value.storage
+      storage: props.value.storageService
     };
     return (
         <ServiceContext.Provider value={value}>
